ff1e9657aa7edd91485b8317ba148b214956e585
Artem Dorofeev <a.dorofeev@ukr.net>
2023-10-17 15:00:02 +0300

    hw07_addition

--- HW_addition/database/db.py
+++ HW_addition/database/db.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.ext.declarative import declarative_base
 import configparser
 import pathlib
 
@@ -8,12 +8,13 @@ file_config = pathlib.Path(__file__).parent.parent.joinpath('config.ini')
 config = configparser.ConfigParser()
 config.read(file_config)
 
-username = config.get('DB', 'user')
-password = config.get('DB', 'password')
-db_name = config.get('DB', 'db_name')
-domain = config.get('DB', 'domain')
+username = config.get('DB', 'USER')
+password = config.get('DB', 'PASSWORD')
+db_name = config.get('DB', 'DB_NAME')
+domain = config.get('DB', 'DOMAIN')
+port = config.get('DB', 'PORT')
 
-url = f'postgresql://{username}:{password}@{domain}:5432/{db_name}'
+url = f'postgresql://{username}:{password}@{domain}:{port}/{db_name}'
 Base = declarative_base()
 engine = create_engine(url, echo=False, pool_size=5)
 
