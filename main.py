6419dc91639fd46b974829ef27b3dd0ee594bda6
Artem Dorofeev <130536391+artem-dorofeev@users.noreply.github.com>
2023-10-30 21:58:10 +0200

    upd main

--- main.py
+++ main.py
@@ -6,17 +6,13 @@ from src.db import session
 
 
 def select_one():
-    """
-    Знайти 5 студентів із найбільшим середнім балом з усіх предметів.
-    :return: list[dict]
-    """
     result = session.query(Student.fullname, func.round(func.avg(Grade.grade), 2).label('avg_grade')) \
         .select_from(Grade).join(Student).group_by(Student.id).order_by(desc('avg_grade')).limit(5).all()
     return result
 
 
 def select_two(discipline_id: int):
-    r = session.query(Discipline.name,
+    result = session.query(Discipline.name,
                       Student.fullname,
                       func.round(func.avg(Grade.grade), 2).label('avg_grade')
                       ) \
@@ -27,21 +23,122 @@ def select_two(discipline_id: int):
         .group_by(Student.id, Discipline.name) \
         .order_by(desc('avg_grade')) \
         .limit(1).all()
-    return r
+    return result
 
-"""
-select s.id, s.fullname, g.grade, g.date_of
-from grades g
-  inner join students s on s.id = g.student_id
-where g.discipline_id = 2
-  and s.group_id = 2
-  and g.date_of = (select max(date_of) -- находим последнее занятие для данной группы по данному предмету
-                   from grades g2
-                     inner join students s2 on s2.id = g2.student_id
-                   where g2.discipline_id = g.discipline_id
-                     and s2.group_id = s.group_id);
 
-"""
+def select_three(discipline_id):
+    result = (
+        session.query(
+            Discipline.name,
+            Group.name,
+            func.round(func.avg(Grade.grade), 2).label("avg_grade"),
+        )
+        .select_from(Grade)
+        .join(Student)
+        .join(Discipline)
+        .join(Group)
+        .filter(Discipline.id == discipline_id)
+        .group_by(Discipline.name, Group.name)
+        .order_by(desc("avg_grade"))
+        .all()
+    )
+
+    return result
+
+
+def select_four():
+    result = (
+        session.query(
+            func.round(func.avg(Grade.grade), 2).label("avg_grade")
+        )
+        .select_from(Grade)
+        .all()
+    )
+    return result
+
+
+def select_five(teacher_id):
+    result = (
+        session.query(
+            Teacher.fullname,
+            Discipline.name
+        )
+        .select_from(Discipline)
+        .join(Teacher)
+        .filter(Discipline.teacher_id == teacher_id)
+        .order_by(desc(Teacher.fullname))
+        .all()
+    )
+    
+    return result
+
+
+def select_six(group_id):
+    result = (
+        session.query(
+            Student.fullname,
+            Group.name
+        )
+        .select_from(Student)
+        .join(Group)
+        .filter(Group.id == group_id)
+        .all()
+    )
+    
+    return result
+
+
+def select_seven(group_id, discipline_id):
+    result = (
+        session.query(
+            Grade.grade,
+            Student.fullname,
+            Group.name,
+            Discipline.name
+        )
+        .select_from(Grade)
+        .join(Student, Grade.student_id == Student.id)
+        .join(Group, Student.group_id == Group.id)
+        .join(Discipline, Grade.discipline_id == Discipline.id)
+        .where(and_(Group.id == group_id, Discipline.id == discipline_id))
+        .order_by(desc(Student.fullname))
+        .all()
+    )
+    return result
+
+
+def select_eight(teacher_id):
+    result = (
+        session.query(
+            Teacher.fullname,
+            Discipline.name,
+            func.round(func.avg(Grade.grade), 2).label("avg_grade")
+        )
+        .select_from(Grade)
+        .join(Discipline)
+        .join(Teacher)
+        .filter(Discipline.teacher_id == teacher_id)
+        .group_by(Discipline.name, Teacher.fullname)
+        .all()
+    )
+    return result
+
+
+def select_nine(student_id):
+    result = (
+        session.query(
+            Discipline.name.label("Discipline"),
+            Student.fullname.label("Student"),
+        )
+        .select_from(Discipline)
+        .join(Grade)
+        .join(Student)
+        .where(Student.id == student_id)
+        .group_by(Discipline.name, Student.fullname)
+        .order_by(Discipline.name)
+        .all()
+    )
+    return result
 
 
 def select_last(discipline_id, group_id):
@@ -68,4 +165,11 @@ def select_last(discipline_id, group_id):
 if __name__ == '__main__':
     print(select_one())
     print(select_two(1))
+    print(select_three(1))
+    print(select_four()[0][0])
+    print(select_five(5))
+    print(select_six(1))
+    print(select_seven(3, 2))
+    print(select_eight(1))
+    print(select_nine(4))
     print(select_last(1, 2))
